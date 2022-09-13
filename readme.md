aabfe05c5f346bf022f7cd68dfdff7304c093e3c
ryu <ryu@anestsystem.jp>
2022-09-13 15:25:02 +0900

    add bootstrap

--- readme.md
+++ readme.md
@@ -1,9 +1,23 @@
 npm init -y  
 npm i jquery -S
 
+npm i bootstrap4   
+npm install popper.js --save
+
+(
+ bootstrap5なら  npm install @popperjs/core
+)
+
+
+
+
+
+
 change package.json to current
 
 npm i
+
+
 
 
 
--- src/js-script.js
+++ src/js-script.js
@@ -1,20 +1,34 @@
 import $ from 'jquery';
-// import {Fireworks} from 'fireworks-js';
+import {Fireworks} from 'fireworks-js';
+import dataObj from '../public/data.json'
+
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Or import only needed plugins
+// import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
+
+// Or import just one
+// import Alert as Alert from '../node_modules/bootstrap/js/dist/alert';
+
+
+// console.log('data: ', data)
+
 
 var msg = '2022-09-08 16:02:23';
 $('#app').html(msg);
 $(function () {
-    $("#btntest").on('click', function () {
-        // const msg = 'abcde';
-        // $('#app').html(msg);
-        // console.log(msg);
-        // $.ajax({
-        //     url: './data.json', type: 'get', dataType: 'json', success: function (data) {
-        //         console.log('data: ', data)
-        //     }
-        //
-        // })
-        // new Fireworks(document.body).start()
+    $("#btntest1").on('click', function () {
+        const msg = 'abcde';
+        $('#app').html(msg);
+       // console.log("dataObj",dataObj);
+        $.ajax({
+            url: './data.json', type: 'get', dataType: 'json', success: function (data) {
+                console.log('data: ', data)
+            }
+
+        })
+        new Fireworks(document.body).start()
 
         console.log(msg);
 
@@ -38,7 +52,7 @@ function f() {
 
     function g() {
         // console.log(msg)
-        debugger; // 在 console 中：输入 alert(value); Surprise!
+        // debugger; // 在 console 中：输入 alert(value); Surprise!
     }
 
     return g;
@@ -48,6 +62,6 @@ let g = f();
 g();
 
 
-// const a: number | undefined = undefined;  // 这里是ts 语法哦
+// const a: number | undefined = undefined;  // 这里是ts 语法哦,不能用
 // const b: number = a!;
 // console.log(b);
